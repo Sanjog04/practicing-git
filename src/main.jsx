0969b533b39454e40ba4d45a74a0db6e70ebc2a4
Sanjoibhoi04 <sanjoibhoi3@gmail.com>
2025-12-04 19:25:55 +0530

    Fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <p>My name is Sanjog</p>
+    <h1>This is a sample message I'm adding for trial purpose.</h1>
   </StrictMode>,
 )
